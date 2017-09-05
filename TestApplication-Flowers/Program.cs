7b87edee19c1f594a47ac0c738455bb2c6bc7f3a
Alina Belova <Alina Belova>
2017-09-05 13:26:22 +0700

    Add project

---
+++ TestApplication-Flowers/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace TestApplication_Flowers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            int k = 0;
+            int x = 0;
+            int i = 0;
+            decimal result = 0;
+            bool condition = false;
+
+            Console.WriteLine("Enter N and K:");
+            string nk = Console.ReadLine();
+            StringHelper.ValidateEmptyString(ref nk, "N and K");
+            string[] NandK = StringHelper.SplitStr(nk, 2, "N and K");
+
+            do
+            {
+                StringHelper.ConverStrToNumber(NandK[0], out n, condition, "N", "N must be greater 0");
+                condition = n < 1;
+            } while (condition);
+
+            do
+            {
+                StringHelper.ConverStrToNumber(NandK[1], out k, condition, "K", "K must be greater 0 and less 101");
+                condition = k < 1 && k >= 100;
+            } while (condition);   
+
+            decimal[] c = new decimal[n];
+
+            Console.WriteLine("Enter C[N]:");
+            string priceNumbers = Console.ReadLine();
+            StringHelper.ValidateEmptyString(ref priceNumbers, "c-numbers");
+            string[] priceNumberSplit = StringHelper.SplitStr(priceNumbers, n, "c-numbers");
+            
+            foreach (string s in priceNumberSplit)
+            {
+                if (s.Trim() != "")
+                {
+                    do
+                    {
+                        StringHelper.ConverStrToNumber(s, out c[i], condition, "C[" + i + "]", "C[" + i + "] must be less or equal 1000,000");
+                        condition = c[i] >= 1000;
+                    } while (condition);
+                    i++;
+                }
+            }
+            
+            for (int j = 0; j < n; j++)
+            {
+                if (j >= k)
+                {
+                    x = j / k;
+                }
+                result += (x + 1) * c[j];
+            }
+            Console.WriteLine("Result: " + result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TestApplication-Flowers/StringHelper.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApplication_Flowers
+{
+    public static class StringHelper
+    {
+        /// <summary>
+        /// Преобразует строку в число
+        /// </summary>
+        /// <param name="numberString">строка, представляющая число</param>
+        /// <param name="number">число, в которое будет преобразована строка</param>
+        /// <param name="condition">условие, проверяющее ограничение числа</param>
+        /// <param name="symbol">обозначение числа</param>
+        /// <param name="error">строка с ошибкой</param>
+        public static void ConverStrToNumber(string numberString, out int number, bool condition, string symbol,
+            string error)
+        {
+            while (true)
+            {
+                if (!Int32.TryParse(numberString, out number))
+                {
+                    Console.Write("Error! Number " + symbol + " can not be converted.");
+                    Console.WriteLine("Please enter " + symbol + " again:");
+                }
+                else if (condition)
+                {
+                    condition = false;
+                    Console.Write("Error: " + error + ". Please enter " + symbol + " again:");
+                }
+                else
+                    break;
+                numberString = Console.ReadLine();
+            }
+        }
+
+        /// <summary>
+        /// Преобразует строку в число
+        /// </summary>
+        /// <param name="numberString">строка, представляющая число</param>
+        /// <param name="number">число, в которое будет преобразована строка</param>
+        /// <param name="condition">условие, проверяющее ограничение числа</param>
+        /// <param name="symbol">обозначение числа</param>
+        /// <param name="error">строка с ошибкой</param>
+        public static void ConverStrToNumber(string numberString, out decimal number, bool condition, string symbol,
+            string error)
+        {
+            while (true)
+            {
+                if (!Decimal.TryParse(numberString, out number))
+                {
+                    Console.Write("Error! Number " + symbol + " can not be converted.");
+                    Console.WriteLine("Please enter " + symbol + " again:");
+                }
+                else if (condition)
+                {
+                    condition = false;
+                    Console.Write("Error: " + error + ". Please enter " + symbol + " again:");
+                }
+                else
+                    break;
+                numberString = Console.ReadLine();
+            }
+        }
+
+        /// <summary>
+        /// Проверяет введенную строку на пустоту
+        /// </summary>
+        /// <param name="validateStr">строка для проверки</param>
+        /// <param name="strInfo">информация о повторном вводе</param>
+        public static void ValidateEmptyString(ref string validateStr, string strInfo)
+        {
+            while (String.IsNullOrEmpty(validateStr))
+            {
+                Console.WriteLine("Error! You entered an empty string.");
+                Console.WriteLine("Please enter " + strInfo + " again:");
+                validateStr = Console.ReadLine();
+            }
+        }
+
+        /// <summary>
+        /// Разделение строки на массив
+        /// </summary>
+        /// <param name="strForSplit">строка, которую необходимо разделить</param>
+        /// <param name="strLength">длина строки</param>
+        /// <param name="strInfo">информация о повторном вводе</param>
+        /// <returns></returns>
+        public static string[] SplitStr(string strForSplit, int strLength, string strInfo)
+        {
+            string[] numberArray = strForSplit.Split(' ', '\t', '\n');
+            while (numberArray.Length != strLength)
+            {
+                Console.WriteLine("Error! You  must enter " + strLength + " numbers.");
+                Console.WriteLine("Please enter " + strInfo + " again:");
+                strForSplit = Console.ReadLine();
+                ValidateEmptyString(ref strForSplit, strInfo);
+                numberArray = strForSplit.Split(' ', '\t', '\n');
+            }
+            return numberArray;
+        }
+    }
+}
